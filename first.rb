e8667d3d7e36cce2a7795bcff10940e5279dd527
dorothnante <ahumuzadora@gmail.com>
2015-02-16 11:55:33 +0300

    second add

--- first.rb
+++ first.rb
@@ -1,4 +1,8 @@
+#puts creates a new line character on output
 puts "welcome to ruby"
+#p outputs everything that is passed on it
 p "welcome to ruby"
+#print outputs everything on the same line
 print "welcome to ruby"
+#puts""creates anew line
 puts""
